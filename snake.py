5f05cac25b6c1798fdd84baaac50c8f370d282fb
Tex <notazioneacammello3@gmail.com>
2022-12-30 17:04:20 +0100

    Aggiunto colore al serpente

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-#snage game taken from https://www.edureka.co/blog/snake-game-with-pygame/
+#snake game taken from https://www.edureka.co/blog/snake-game-with-pygame/
 import pygame
 import time
 import numpy as np
@@ -13,37 +13,53 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+brown = (78, 53, 36)
+blue = (0, 0, 255)
+color_list = list()
  
-dis_width = 800
-dis_height = 800
+dis_width = 1000
+dis_height = 1000
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake')
  
 clock = pygame.time.Clock()
- 
+font_style = pygame.font.SysFont(None, 170)
+
 snake_block = 50
 snake_speed = 10
 
-MAX_TEST_NUMBER = 400
-MAX_STEP = 800
+MAX_TEST_NUMBER = 100 #400
+MAX_STEP = 400 #800
 
 def euclidean(a, b):
     return np.sqrt(pow(a[0]-b[0],2) + pow(a[1]-b[1],2))
 
 def manhattan(a, b):
     return sum(abs(val1-val2) for val1, val2 in zip(a,b))
- 
+
+def writeTraining():
+    msg = font_style.render("TRAINING", True, red)
+    text_rect = msg.get_rect(center=(dis_width/2, dis_height/2))
+    dis.blit(msg, text_rect)
+
+def drawFood(foodx, foody, snake_block):
+    # pygame.draw.rect(dis, red, [foodx+snake_block*3/40, foody+snake_block*3/40, snake_block*17/20, snake_block*17/20])
+    pygame.draw.ellipse(dis, red, [foodx, foody+snake_block/20, snake_block, snake_block*9/10])
+    pygame.draw.rect(dis, brown, [foodx+snake_block*2/5, foody, snake_block/5, snake_block/4])
+
 def drawSnake(snake_block, snake_list):
-    pygame.draw.rect(dis, black, [snake_list[len(snake_list)-1][0], snake_list[len(snake_list)-1][1], snake_block, snake_block])
+    pygame.draw.rect(dis, color_list[len(color_list)-1], [snake_list[len(snake_list)-1][0], snake_list[len(snake_list)-1][1], snake_block, snake_block])
     if len(snake_list) > 1:
         maxGr = 9/10
         minGr = 2/5
         stepGr = (maxGr - minGr) / (len(snake_list)-1)
         dimTail = minGr
         dist = (1-dimTail)/2
+        color_index = 0
         for x in snake_list[:-1]:
-            pygame.draw.rect(dis, black, [x[0]+snake_block*dist, x[1]+snake_block*dist, snake_block*dimTail, snake_block*dimTail])
+            pygame.draw.rect(dis, color_list[color_index], [x[0]+snake_block*dist, x[1]+snake_block*dist, snake_block*dimTail, snake_block*dimTail])
+            color_index = color_index + 1
             dimTail = dimTail + stepGr
             dist = (1-dimTail)/2
 
@@ -111,9 +127,11 @@ def getChangePosition(action):
         y1_change = 0
     return (x1_change, y1_change)
 
-def gameLoop(watchResult=False):
+def gameLoop(isTraining=False):
     game_over = False
- 
+    color_list.clear()
+    color_list.append(list(np.random.choice(range(256), size=3)))
+
     x1 = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
     y1 = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
  
@@ -131,7 +149,7 @@ def gameLoop(watchResult=False):
         precx1 = x1
         precy1 = y1
         state = getState(x1, y1, snake_list, foodx, foody)
-        action = table.chooseAction(state, not watchResult)
+        action = table.chooseAction(state, isTraining)
         x1_change, y1_change = getChangePosition(action)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -143,8 +161,6 @@ def gameLoop(watchResult=False):
         snake_head.append(y1)
         snake_list.append(snake_head)
         reward = getReward(precx1, precy1, x1, y1, snake_list, snake_head, foodx, foody)
-        #if watchResult:
-            #print(reward)
         temp_x1, temp_y1 = getChangePosition(table.chooseAction(getState(x1, y1, snake_list, foodx, foody),  False))
         temp_x1 = temp_x1 + x1
         temp_y1 = temp_y1 + y1
@@ -158,29 +174,30 @@ def gameLoop(watchResult=False):
         if len(snake_list) > length_of_snake:
             del snake_list[0]
         
-        dis.fill(white)
-        pygame.draw.rect(dis, red, [foodx+snake_block*1/10, foody+snake_block*1/10, snake_block*4/5, snake_block*4/5])
-        drawSnake(snake_block, snake_list)
-        pygame.display.update()
- 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
             foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
             while [foodx, foody] in snake_list:
                 foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
                 foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+            color_list.append(list(np.random.choice(range(256), size=3)))
             length_of_snake += 1
-        if watchResult:
+
+        dis.fill(white)
+        drawFood(foodx, foody, snake_block)
+        drawSnake(snake_block, snake_list)
+        if isTraining:
+            writeTraining()
+        pygame.display.update()
+        if not isTraining:
             clock.tick(snake_speed)
-    
-    print("Max step")
+
 
 def run():
     n_test = 0
     while n_test < MAX_TEST_NUMBER:
         n_test = n_test + 1
-        gameLoop(False)
-    gameLoop(True)
-    table.printTable()
+        gameLoop(True)
+    gameLoop(False)
 
 run()
